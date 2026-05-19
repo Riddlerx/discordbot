6b7ebbf8a4289e6abb17cfaeed0b8455c492d56e
Riddler <winhtutooheart2@gmail.com>
2026-05-19 05:40:57 +0000

    feat: support YTDLP_REMOTE_COMPONENTS env var

--- music.py
+++ music.py
@@ -186,7 +186,11 @@ def _build_ydl_options(base_options: dict) -> dict:
 
     js_runtime = os.getenv("YTDLP_JS_RUNTIME")
     if js_runtime:
-        options["js_runtimes"] = {js_runtime: {}}
+        options["js_runtimes"] = [js_runtime]
+
+    remote_components = os.getenv("YTDLP_REMOTE_COMPONENTS")
+    if remote_components:
+        options["remote_components"] = remote_components
 
     if auth_cfg.get("cookiefile"):
         logger.info("Using yt-dlp cookies from %s", auth_cfg["cookiefile"])
